97883db032e2ec69f15d341225e4c6fa26896eac
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-03-04 17:12:14 +0600

    fitst commit

--- switch.js
+++ switch.js
@@ -13,3 +13,29 @@ var number = 43;
     default:
         console.log("ami nai")
  }
+
+//  var jaka = 11;
+//  if(jaka === 10){
+//     console.log("tomir number soto na")
+//  }
+//  else if(jaka === 10){
+//     console.log('aita amir number')
+//  }
+//  else{
+//     console.log("tomider karo number na ai ta difuld naumber")
+//  }
+ function tumikar (){
+    var jaka = 2;
+ if(jaka === 10){
+    console.log("tomir number soto na")
+ }
+ else if(jaka === 1){
+    console.log('aita amir number')
+ }
+ else{
+    console.log("tomider karo number na ai ta difuld naumber")
+ }
+ }
+ tumikar();
+ tumikar();
+ 
